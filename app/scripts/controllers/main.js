03da543bf33eb9e9679ebb0e52ef6f474f2f26c0
Thomas Beckenhauer <thomasbeckenhauer@gmail.com>
2018-04-06 17:51:44 -0400

    init project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name memoryGameApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the memoryGameApp
+ */
+angular.module('memoryGameApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
