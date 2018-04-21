7238e6bd8f300b37359228ad187f1a3215d6f5d8
Thomas Beckenhauer <thomasbeckenhauer@gmail.com>
2018-04-21 18:52:16 -0400

    Feat: improve tmbJsonPrinter

--- app/components/tmbJsonPrinter.js
+++ app/components/tmbJsonPrinter.js
@@ -5,10 +5,47 @@
  * @name demoApp.directive:tmbJsonPrinter
  * @description
  * # AboutCtrl
- * Directive capable of printing out json as nested unordered lists.
+ * Directive capable of printing out json.
+ * obj -> ul + li
+ * arr -> ol + li
+ * key/val -> dl dt dd
  */
-angular.module('demoApp').directive('tmbJsonPrinter', function(Cards, Shuffle) {
+angular.module('demoApp').directive('tmbJsonPrinter', function(Cards, Shuffle, Lodash) {
     return {
-        template: '<ul><li ng-repeat="(k,v) in obj">{{k}} {{v}}</li></ul>'
+        scope: {
+            obj: '=obj'
+        },
+        replace: true,
+        template: '<data class="tmbJsonPrinter"></data>',
+        link: function (scope, element, attrs, controller, transcludeFn) {
+            scope.$watch('obj', function() {
+                attachObjToElement(element, 'obj', scope.obj);
+            });
+
+            function attachObjToElement (elem, key, value) {
+                let curElem;
+                if(Lodash.isObject(value)) {
+                    curElem = angular.element(elem).append('<ul></ul>');
+                    curElem = angular.element('ul', curElem);
+                    Object.keys(value).forEach((k) => {
+                        attachObjToElement(curElem, k, value[k]);
+                    });
+                } else if (Lodash.isNumber(value)) {
+                    attachLi(elem, key, value);
+                } else if (Lodash.isString(value)) {
+                    attachLi(elem, key, value);
+                }
+            }
+
+            function attachLi(elem, key, value) {
+                angular.element(elem).append(`
+                    <li>
+                        <dl>
+                            <dt>${key}</dt>
+                            <dd>${value}</dd>
+                        </dl>
+                    </li>`)
+            }
+        }
     };
 });
--- app/components/tmbJsonPrinter.spec.js
+++ app/components/tmbJsonPrinter.spec.js
@@ -3,7 +3,6 @@
 describe('Directive: tmpJsonPrinter', function () {
     var $compile,
         $rootScope,
-        makeNewScope,
         makeNewDirective;
 
     beforeEach(function () {
@@ -25,25 +24,95 @@ describe('Directive: tmpJsonPrinter', function () {
     });
 
     it('should return an empty string if no json object is provided', function() {
-        var element = makeNewDirective('<tmb-json-printer></tmb-json-printer>')
-        expect(angular.element('ul', element).length).toBe(1);
+        var element = makeNewDirective('<tmb-json-printer></tmb-json-printer>');
+        expect(angular.element(element).is('data')).toBe(true);
     });
 
-    it('should print out the key and value of an object as an li tag', function() {
-        var element = makeNewDirective('<tmb-json-printer></tmb-json-printer>', {
-            obj: { 'Key 1': 'Value 1' }
+    describe('Number to li', () => {
+        it('should print out a li tag for each key value<Number> pair of an object', function() {
+            var element = makeNewDirective('<tmb-json-printer obj="obj"></tmb-json-printer>', {
+                obj: {
+                    a: 1,
+                }
+            });
+            expect(angular.element('li', element).length).toBe(1);
         });
-        expect(angular.element('li', element).length).toBe(1);
     });
 
-    it('should print out the keys and values of an object as li tags', function() {
-        var element = makeNewDirective('<tmb-json-printer></tmb-json-printer>', {
-            obj: {
-                'Key 1': 'Value 1',
-                'Key 2': 'Value 2',
-            }
+    describe('String to li', () => {
+        it('should print out a li tag for each key value<String> pair of an object', function() {
+            var element = makeNewDirective('<tmb-json-printer obj="obj"></tmb-json-printer>', {
+                obj: {
+                    'Favorite Book': 'The Cat in the Hat',
+                }
+            });
+            expect(angular.element('li', element).length).toBe(1);
         });
-        expect(angular.element('li', element).length).toBe(2);
-    });
+    })
 
+    describe('objects to ul', () => {
+        it('should print out an Object as an unordered list', () => {
+            var element = makeNewDirective('<tmb-json-printer obj="obj"></tmb-json-printer>', {
+                obj: { }
+            });
+            expect(angular.element('ul', element).length).toBe(1);
+        });
+
+        it('should recursively print objects with ul', function() {
+            var element = makeNewDirective('<tmb-json-printer obj="obj"></tmb-json-printer>', {
+                obj: {
+                    a: {
+                        b: {}
+                    },
+                }
+            });
+            expect(angular.element('ul', element).length).toBe(3);
+        });
+
+        it('should recursively print out li tags for each key value<String> pair of an object', function() {
+            var element = makeNewDirective('<tmb-json-printer obj="obj"></tmb-json-printer>', {
+                obj: {
+                    a: {
+                        b: {
+                            'Favorite Book3': 'The Cat in the Hat3',
+                        },
+                        'Favorite Book2': 'The Cat in the Hat2',
+                    },
+                    'Favorite Book1': 'The Cat in the Hat',
+                }
+            });
+            expect(angular.element('ul', element).length).toBe(3);
+            expect(angular.element('li', element).length).toBe(3);
+        });
+
+        it('should recursively print out li tags for each key value<String> pair of an object', function() {
+            var element = makeNewDirective('<tmb-json-printer obj="obj"></tmb-json-printer>', {
+                obj: {
+                    'Favorite Book1': 'The Cat in the Hat',
+                    a: {
+                        'Favorite Book2': 'The Cat in the Hat2',
+                        b: {
+                            'Favorite Book3': 'The Cat in the Hat3',
+                        },
+                    },
+                }
+            });
+            expect(angular.element('li', element).length).toBe(3);
+        });
+
+        it('should recursively print out li tags for each key value<String> pair of an object', function() {
+            var element = makeNewDirective('<tmb-json-printer obj="obj"></tmb-json-printer>', {
+                obj: {
+                    'Favorite Book1': 'The Cat in the Hat',
+                    'Favorite Book2': 'The Cat in the Hat2',
+                    a: {
+                        'Favorite Book3': 'The Cat in the Hat3',
+                        'Favorite Book4': 'The Cat in the Hat4',
+                    },
+                }
+            });
+            expect(angular.element('li', element).length).toBe(4);
+        });
+
+    });
 });
